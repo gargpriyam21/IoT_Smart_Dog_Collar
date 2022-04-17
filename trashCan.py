776517cb00411beb1ce1bb8dc4380f78248419d1
Priyam Garg <gargpriyam21@gmail.com>
2022-04-17 18:08:32 -0400

    Add files via upload

---
+++ trashCan.py
@@ -0,0 +1,81 @@
+import sys
+import time
+from mpu6050 import mpu6050
+import numpy as np
+import time
+
+import paho.mqtt.client as client
+
+BROKER = '192.168.86.21'
+PORT = 4000
+KEEP_ALIVE = 100
+
+topic_send = "ncsu/iot/TrashCanInDanger"
+
+topic_position = "ncsu/iot/DogPosition"
+
+mqtt_dog_publisher = client.Client()
+mqtt_dog_publisher.connect(BROKER, port=PORT)
+
+
+
+
+mpu = mpu6050(0x68)
+threshold = 10 #must be above threshold to move dog from "idle" to "moving" state
+IMU_SAMPLING_RATE = 0.01 #seconds
+
+#Calibrate sensors to account for acceleration due to gravity
+def calibrate(sensors):
+    #get 5 samples from x,y and z coordinates
+    x = np.zeros(5)
+    y = np.zeros(5)
+    z = np.zeros(5)
+    for i in range(0,5):
+        x[i] = sensors.get_accel_data().get('x')
+        y[i] = sensors.get_accel_data().get('y')
+        z[i] = sensors.get_accel_data().get('z')
+        time.sleep(IMU_SAMPLING_RATE)
+    x_offset = np.mean(x)
+    y_offset = np.mean(y)
+    z_offset = np.mean(z)
+    return [x_offset,y_offset,z_offset]
+
+def get_mag(sensor, offset):
+    vals = [sensor.get_accel_data().get('x'), sensor.get_accel_data().get('y')]
+    mag = 0
+    for i in range(0,2):
+        mag = mag + ((vals[i]-offset[i])**2)
+    return np.sqrt(mag)
+
+
+def main():
+    mag = [0,0,0]
+    status = "Idle"
+    #Publish status
+    offset = calibrate(mpu)
+    while True:
+        mag[2] = mag[1]
+        mag[1] = mag[0]
+        mag[0] = get_mag(mpu,offset)
+        avg_mag = np.mean(mag)
+        if (avg_mag > threshold) & (status == "Idle"):
+            status = "Bumped"
+            #Publish status
+            mqtt_dog_publisher.publish(topic_send,status)
+            print("Trashcan Bumped")
+            time.sleep(3)
+        elif (avg_mag < threshold) & (status == "Bumped"):
+            status = "Idle"
+            mqtt_dog_publisher.publish(topic_send,status)
+
+            #Publish status
+            print("Trashcan not moving")
+            
+        time.sleep(IMU_SAMPLING_RATE)
+if __name__ == '__main__':
+    main()
+    
+
+
+
+
