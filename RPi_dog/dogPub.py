0b63d03848e5078ddfba72b2bd01c398fdae2d8e
Brendan <bhdrisco@ncsu.edu>
2022-04-18 16:52:05 -0400

    Cleaned up logic in dogPub.py, now works consistently

--- RPi_dog/dogPub.py
+++ RPi_dog/dogPub.py
@@ -5,8 +5,7 @@ import time
 
 mpu = mpu6050(0x68)
 move_threshold = 3 #must be above threshold to move dog from "idle" to "moving" state
-idle_threshold = 0.5 #must be below threshold to move dog from "moving" to "idle" state
-gyro_threshold = 100 #threshold to recalibrate MPU if dog rolls over
+idle_threshold = 2 #must be below threshold to move dog from "moving" to "idle" state
 IMU_SAMPLING_RATE = 0.01 #seconds
 BROKER = '192.168.1.163'
 PORT = 4000
@@ -17,59 +16,52 @@ topic_send = "ncsu/iot/DogWalkingStatus"
 mqtt_dog_publisher = client.Client()
 mqtt_dog_publisher.connect(BROKER, port=PORT)
 
-#Calibrate sensors to account for acceleration due to gravity
-def calibrate(sensors):
-	#get 5 samples from x,y and z coordinates
-	x = np.zeros(5)
-	y = np.zeros(5)
-	z = np.zeros(5)
-	for i in range(0,5):
-		x[i] = sensors.get_accel_data().get('x')
-		y[i] = sensors.get_accel_data().get('y')
-		z[i] = sensors.get_accel_data().get('z')
-		time.sleep(IMU_SAMPLING_RATE)
-	x_offset = np.mean(x)
-	y_offset = np.mean(y)
-	z_offset = np.mean(z)
-	return [x_offset,y_offset,z_offset]
 
-def get_mag(sensor, offset):
-	vals = [sensor.get_accel_data().get('x'), sensor.get_accel_data().get('y')]
-	mag = 0
-	for i in range(0,2):
-		mag = mag + ((vals[i]-offset[i])**2)
-	return np.sqrt(mag)
+def get_mag(sensor):
+    vals = [sensor.get_accel_data().get('x'), sensor.get_accel_data().get('y'), sensor.get_accel_data().get('z')]
+    mag = 0
+    for i in range(0,3):
+        #mag = mag + ((vals[i]-offset[i])**2)
+        mag = mag + ((vals[i])**2)
+    return np.sqrt(mag)
 
-def get_rot(sensor):
-	rot = [sensor.get_gyro_data().get('x'), sensor.get_gyro_data().get('y'), sensor.get_gyro_data().get('z')]
-	return rot
+"""
+Main loop logic:
+-Determines dog status using the "diff" variable
+-diff is the difference between the accelerometer at rest (rest_val) and "avg_mag"
+-avg_mag is a moving average of the accelerometer values
+-Once the dog's status changes to "DogMoving" it will remain in this state until he remains still for approximatley 3 seconds
+	-rest_counter keeps track of how long the dog has been still
+	-rest_counter is reset if the dog moves again
+
+"""
 
 def main():
-	mag = [0,0,0]
-	diff = np.zeros(3)
-	status = "DogStill"
-	offset = calibrate(mpu)
-	while True:
-		mag[2] = mag[1]
-		mag[1] = mag[0]
-		mag[0] = get_mag(mpu,offset)
-		avg_mag = np.mean(mag)
-		for i in range(0,3):
-			diff[i-3] = diff[i-2]
-		diff[0] = mag[0] - avg_mag
-		avg_diff = np.mean(diff)
+    mag = [0,0,0]
+    status = "DogStill"
+    while True:
+        mag[2] = mag[1]
+        mag[1] = mag[0]
+        mag[0] = get_mag(mpu)
+        avg_mag = np.mean(mag)
+        diff = np.abs(rest_val - avg_mag)
+        if (diff > move_threshold):
+            rest_counter = 0
+            if (status == "DogStill"):
+                status = "DogMoving"
+				mqtt_dog_publisher.publish(topic_send,status)
+                print("Dog is moving")
+        elif (diff < idle_threshold) & (status == "DogMoving"):
+            rest_counter = rest_counter+1
+            #print(rest_counter)
+            if rest_counter >= 300:
+                status = "DogStill"
+				mqtt_dog_publisher.publish(topic_send,status)
+                print("Dog is idle")
 
-		if (avg_diff > move_threshold) & (status == "DogStill"):
-			status = "DogMoving"
-			mqtt_dog_publisher.publish(topic_send,status)
-			print("Dog is moving")
-		elif (avg_diff < idle_threshold) & (status == "DogMoving"):
-			status = "DogStill"
-			mqtt_dog_publisher.publish(topic_send,status)
-			print("Dog is idle")
-		#publish status to dog topic
-		time.sleep(IMU_SAMPLING_RATE)
-		#print(avg_diff)
+        time.sleep(IMU_SAMPLING_RATE)
+        #print(diff)
+
 if __name__ == '__main__':
     main()
 
