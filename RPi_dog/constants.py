c6d2794ac49ddd2df16fabcf6e01b16fdf197e61
Brendan <bhdrisco@ncsu.edu>
2022-04-21 05:24:17 -0400

    tweaked rest_val and tested the code

--- RPi_dog/constants.py
+++ RPi_dog/constants.py
@@ -2,10 +2,10 @@ move_threshold = 3 #must be above threshold to move dog from "idle" to "moving" 
 idle_threshold = 2 #must be below threshold to move dog from "moving" to "idle" state
 IMU_SAMPLING_RATE = 0.01 #seconds
 BROKER = '192.168.1.163'
-PORT = 4000
+PORT = 1883
 KEEP_ALIVE = 100
 
 #TODO: Brendan
-rest_val = 50
+rest_val = 8
 
 topic_send = "ncsu/iot/DogWalkingStatus"
