abdc363552d14c08984e5f612427b1e9c561538a
Divyag Doshi <divyangdoshi02@gmail.com>
2022-04-27 01:21:34 -0400

    Fixing dogPub code

--- RPi_dog/constants.py
+++ RPi_dog/constants.py
@@ -1,9 +1,12 @@
-move_threshold = 5 #must be above threshold to move dog from "idle" to "moving" state
-idle_threshold = 1.5 #must be below threshold to move dog from "moving" to "idle" state
-IMU_SAMPLING_RATE = 0.01 #delay in seconds between samples
+# 
+move_threshold = 3 #must be above threshold to move dog from "idle" to "moving" state
+idle_threshold = 1 #must be below threshold to move dog from "moving" to "idle" state
+IMU_SAMPLING_RATE = 0.01 #seconds
 BROKER = '192.168.1.163'
 PORT = 1883
 KEEP_ALIVE = 100
 
+#TODO: Brendan
+rest_val = 8
 
 topic_send = "ncsu/iot/DogWalkingStatus"
--- RPi_dog/dogPub.py
+++ RPi_dog/dogPub.py
@@ -1,13 +1,21 @@
+# 
 import paho.mqtt.client as client
 from mpu6050 import mpu6050
 import numpy as np
 import time
 from constants import *
 from helpers import *
-import sys
+# from Triangulation.triangulation import *
 
-# sys.path.insert(1, '/home/pi/Documents/IoT_Final_Group11/Triangulation/')
-# from triangulation import *
+
+def get_mag(sensor):
+    acceleration_data = sensor.get_accel_data()
+    vals = [acceleration_data.get('x'), acceleration_data.get('y'), acceleration_data.get('z')]
+    mag = 0
+    for i in range(0, 3):
+        # mag = mag + ((vals[i]-offset[i])**2)
+        mag = mag + ((vals[i]) ** 2)
+    return np.sqrt(mag)
 
 """
 Main loop logic:
@@ -17,50 +25,50 @@ Main loop logic:
 -Once the dog's status changes to "DogMoving" it will remain in this state until he remains still for approximatley 3 seconds
     -rest_counter keeps track of how long the dog has been still
     -rest_counter is reset if the dog moves again
-
 """
 
 def calculate_dog_status(diff, mqtt_dog_publisher, rest_counter, status):
     if diff > move_threshold and status == "DogStill":
         rest_counter[0] = 0
         status = "DogMoving"
-        mqtt_dog_publisher.publish(topic_send, status)
+        mqtt_dog_publisher.publish(topic_send, status,qos = 2)
         print("Dog is moving")
     elif (diff < idle_threshold) & (status == "DogMoving"):
         rest_counter[0] = rest_counter[0] + 1
         # print(rest_counter)
-        if rest_counter[0] >= 3:
-            time.sleep(0.25)
+        if rest_counter[0] == 300:
+            time.sleep(0.01)
             status = "DogStill"
-            mqtt_dog_publisher.publish(topic_send, status)
-            rest_counter[0] = 0
+            mqtt_dog_publisher.publish(topic_send, status,qos = 2)
             print("Dog is idle")
+            rest_counter[0] = 0
     return status
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to the Broker with result code "+str(rc))
+    client.publish(topic_send, "DogStill",qos = 2)
+    print("Dog is idle")
+
 
 def main():
+    mag = [rest_val]*3
 
-    mag = [0]*3
-    mpu = mpu6050(0x68)
+
     status = "DogStill"
-    offset = calibrate(mpu)
-
     mqtt_dog_publisher = client.Client("RPi_Dog")
     mqtt_dog_publisher.on_connect = on_connect
-    mqtt_dog_publisher.connect(BROKER, port=PORT, keepalive=2000)
+    mqtt_dog_publisher.connect(BROKER, port=PORT,keepalive=2000)
     mqtt_dog_publisher.loop_start()
 
+    mpu = mpu6050(0x68)
     rest_counter = [0]
 
     while True:
-        mag_mpu = get_mag(mpu, offset)
+        mag_mpu = get_mag(mpu)
         avg_mag = find_moving_average(mag, mag_mpu)
-        diff = np.abs(avg_mag)
+        diff = np.abs(rest_val - avg_mag)
 
         status = calculate_dog_status(diff, mqtt_dog_publisher, rest_counter, status)
-
 
         time.sleep(IMU_SAMPLING_RATE)
     # print(diff)
